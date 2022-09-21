f4079a3c73122a790685ca35e1cefe5d2583c157
aleganza <ganzarolli.alessio@gmail.com>
2022-09-21 11:11:42 +0200

    primi.c

---
+++ code/primi.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int cnt = 0;
+
+    printf("Stampa numeri primi da 1 a 10:\n");
+
+    for (int i=1; i<=10; i++){
+        for (int j=1; j<=i; j++){
+            if (i%j == 0) {
+                cnt++;
+            }
+        }
+
+        if (cnt <= 2)
+            printf ("%d ", i);
+        else
+            printf ("_ ");
+
+        cnt = 0;
+    }
+
+    return 0;
+}
