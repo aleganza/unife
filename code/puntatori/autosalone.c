8c550cd8837963c9d7a7aec746096de5df385904
aleganza <ganzarolli.alessio@gmail.com>
2022-12-15 01:45:50 +0100

    Update autosalone.c

--- code/puntatori/autosalone.c
+++ code/puntatori/autosalone.c
@@ -10,30 +10,140 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
-#define DIM 30
+#define SLEN 30
+#define DIM 5
 
 typedef struct {
-    char nome[DIM];
-    char cognome[DIM];
+    char nome[SLEN];
+    char cognome[SLEN];
 } acquirente;
 
 typedef struct {
-    char marca[DIM];
+    char marca[SLEN];
     int cilindrata;
     int annoImmatricolazione;
-    acquirente a;
+    acquirente acq;
 } automobili;
 
+void inserisciVetture(automobili *s) {
+    for (int i=0; i<DIM; i++) {
+        printf ("Inserire nome del proprietario: ");
+        scanf ("%s", s[i].acq.nome);
+
+        printf ("Inserire cognome del proprietario: ");
+        scanf ("%s", s[i].acq.cognome);
+        
+        printf ("Inserire marca della vettura: ");
+        scanf ("%s", s[i].marca);
+        
+        do {
+            printf ("Inserire cilindrata della vettura: ");
+            scanf ("%d", &s[i].cilindrata);
+        } while (s[i].cilindrata < 800 || s[i].cilindrata > 2500);
+        
+        do {
+            printf ("Inserire anno di immatricolazione della vettura: ");
+            scanf ("%d", &s[i].annoImmatricolazione);
+        } while (s[i].annoImmatricolazione < 2000 || s[i].annoImmatricolazione > 2019);
+
+        printf ("\n");
+    }
+}
+
+// void stampaTutto(automobili *s) {
+//     for (int i=0; i<DIM; i++) {
+//         printf ("%s\n", s[i].acq.nome);
+//         printf ("%s\n", s[i].acq.cognome);
+//         printf ("%s\n", s[i].marca);
+//         printf ("%d\n", s[i].cilindrata);
+//         printf ("%d\n", s[i].annoImmatricolazione);
+//         printf ("\n");
+//     }
+// }
+
+void stampaInfo(automobili *s, int index) {
+    printf ("%s\n", s[index].acq.nome);
+    printf ("%s\n", s[index].acq.cognome);
+    printf ("%s\n", s[index].marca);
+    printf ("%d\n", s[index].cilindrata);
+    printf ("%d\n", s[index].annoImmatricolazione);
+    printf ("\n");
+}
+
+void ricercaMarca(automobili *s, char* marca) {
+    int cnt = 0;
+
+    printf ("Vetture trovate:\n\n");
+
+    for (int i=0; i<DIM; i++) {
+        if (!strcmp(s[i].marca, marca)) {
+            stampaInfo(s, i);
+            cnt++;
+        }
+    }
+
+    // se non trovo nessuna info lo comunico
+    if (!cnt) {
+        printf ("Nessuna vettura trovata.");
+    }
+}
+
+// !! Stampa solo il cognome del proprietario !!
+void ricercaCilindrata(automobili *s, int *cilindrata) {
+    int cnt = 0;
+
+    printf ("Proprietari trovati:\n\n");
+
+    for (int i=0; i<DIM; i++) {
+        if (s[i].cilindrata > *cilindrata) {
+            printf ("%s\n", s[i].acq.cognome);
+            cnt++;
+        }
+    }
+
+    if (!cnt) {
+        printf ("Nessun proprietario trovata.");
+    }
+}
+
+// !! Stampa il numero di auto !!
+void ricercaAnno(automobili *s, int *anno) {
+    int cnt;
+
+    for (int i=0; i<DIM; i++) {
+        if (s[i].annoImmatricolazione == *anno) {
+            cnt++;
+        }
+    }
+
+    printf ("Sono state trovate %d auto", cnt);
+}
+
 int main() {
-    int scelta;
+    automobili autosalone[DIM];
+    char marca[SLEN];
+    int cilindrata, 
+        anno;
 
-    do {
-        printf ("--- AUTOSALONE ---\n\n");
-        printf ("")
+    printf ("--- AUTOSALONE ---\n\n");
+    printf ("Inserimento vetture:\n");
 
+    inserisciVetture(autosalone);
 
-    } while (scelta != 0);
+    printf("Ricerca per marca: ");
+    scanf ("%s", &marca);
+    ricercaMarca(autosalone, marca);
+    
+    // printf ("Ricerca per cilindrata");
+    // scanf ("%d", &cilindrata);
+    cilindrata = 1800;
+    ricercaCilindrata(autosalone, &cilindrata);
+
+    printf ("Ricerca per anno: ");
+    scanf ("%d", &anno);
+    ricercaAnno(autosalone, &anno);
     
     printf ("\n");
     return 0;
