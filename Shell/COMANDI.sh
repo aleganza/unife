25ae861f5eb11af04aef9df62930698f07e5d1e6
aleganza <ganzarolli.alessio@gmail.com>
2023-11-02 17:31:24 +0100

    comandi

--- Shell/COMANDI.sh
+++ Shell/COMANDI.sh
@@ -4,6 +4,17 @@
     • invece di ` ... ` → $( ... )
     AltGr + ì => ~
 
+# PASSAGGIO PARAMETRi
+    ./nomefile arg1 arg2 # $0 = /nomefile
+                         # $1 = arg1
+                         # $2 = arg12
+    shift # comando che sposta i parametri verso sinistra (tranne $s0 che rimane fisso), ex dopo shift: $1 => $2 
+    set # riassegnamento di argomenti
+    $* # insieme di tutti gli parametri da $1
+    $# # numero di parametri
+    $? # valore (int) restituito dall'ultimo comando eseguito (utile per error handling)
+    $$ id numerico del processo in esecuzione (pid)
+
 # COSTANTI GLOBALI
     BASH=/usr/bin/bash
     HOME=/space/home/wwwlia/www
@@ -15,11 +26,15 @@
     UID=1015
     USER=cgiannelli
 
-# VARIABILI
+# VARIABILI E METACARATTERI
     X=2 # creazione e assegnazione variabile
         echo $X # ex: per far riferimento si usa il $
-
-# METACARATTERI
+    expr # le variabili sono tutte string, per fare operazioni si usa questa comando
+        `expr $var + 1` # ex: in questo modo $var e 1 vengono considerati come valori
+    eval # esegue un'ulteriore sostituzione (quando viene bloccata per esempio da '')
+        y=3
+        x='$y'
+        eval echo $x # ex: stampa 3 (perché valuta $y, altrimenti senza eval avrebbe stampato "$y", cioè il valore di x)
     * # qualunque stringa di >= 0 caratteri in un nome di un file
     ? # qualunque carattere in un nome di un file
     [zfc] # qualunque carattere compreso nel range dato
@@ -27,10 +42,73 @@
         [a-p,1-7]*[c,f,d]? # ex: qualunque file che inizia per a-p oppure 1-7, il cui penultimo carattere sia c, f, d
     \  # escape, segnala di non interpretare il carattere successivo come speciale
         *\** # ex: qualunque file che contenga il carattere "*"
+    '' # proteggono da tutte le espansioni
+    "" # proteggono dalle espansioni tranne $, \, `` 
+        echo ' * e $y ' # ex: produce * e $y
+        echo " * e $y " # ex: produce * e 3
+        echo "`pwd`" # ex: stampa nome dir corrente
+
+# STRUTTURE
+    # if else
+    if <lista-comandi>
+        then
+            <comandi>
+        [elif <lista_comandi>
+            then <comandi>]
+        [else <comandi>]
+    fi
+
+    # switch case
+    case <var> in
+        <pattern-1>)
+            <comandi>;;
+        ...
+        <pattern-i> | <pattern-j> | <pattern-k>)
+            <comandi>;;
+        ...
+        <pattern-n>)
+            <comandi> ;;
+    esac
+
+    # ciclo for
+    for <var> [in <list>] # list=lista di stringhe
+    do
+        <comandi>
+    done
+
+        # esempi di ciclo for
+        for i # cicla per $*
+        for i in * # cicla per tutti i file nella cartella corrente
+        for i in `cat file` # cicla per ogni parola in file
+
+    # ciclo while
+    while <lista-comandi> # occhio! esce quando non è più zero!
+    do
+        <comandi>
+    done
+
+    until <lista-comandi> # come while, ma inverte la condizione
+    do
+        <comandi>
+    done
+
+    # uscite anomale
+    exit [status]
+
+    # menu testuale
+    select # # capire bene come si usa
 
 # GENERALI
     man comando # informazioni sul comando in questione
     echo <stringa> # stampa
+    test -<opzioni> <nomefile> # valutazione di un'espressione
+        valore zero ➔ true
+        valore non-zero ➔ false
+        -f <file> # esistenza di file
+        -d <file> # esistenza di cartelle
+        -r <file> # dirito di lettura su file (-w e -x)
+        test <stringa1> = <stringa2> # uguaglianza stringhe (spazi tra = necessari!)
+        test <stringa1> != <stringa2> # diversità stringhe (spazi tra != necessari!)
     cd ~ # vai alla root delle cartelle
     pwd # mostra PATH assoluta della dir corrente
     sudo comando # comando con amministratore
@@ -73,7 +151,16 @@
         -n # interpreta le righe del file come numeri
         -k <n> # ordina file secondo n-esima colonna
     rev <file> # inverte l'ordine delle linne di un file
-    cut [-options] <file> # seleziona colonne da file
+    cut [-options] <file> # seleziona parti di una stringa o di righe di file
+        -c # seleziona i caratteri nelle posizioni specificate
+        -f # divide la stringa in più campi e seleziona il campo
+        -d # usato con -f, comunica il carattere da cui dividere
+            echo $s | cut -f 2 -d `,` # ex: prende la seconda parte della stringa divisa per ,
+    tr [-options] # sostituisce caratteri di una stringa o di righe di file con un altro carattere (anche più caratteri)
+        echo $str | tr ",:" " ," # ex: sotituisce "," con " " e ":" con ","
+        -d # rimuove direttamente il/i carattere/i
+    seq <sequenzanumeri> # stampa sequenze di numeri
+    find # cerca file con un certo criterio PRENDERE SA SLIDE COME SI USA
     diff <file1> <file2> # mostra righe diverse tra due file
     wc # stampa il numero di roba (guardare sotto)
         -l <file> # conta le linee
@@ -95,4 +182,4 @@
         a # mostra i processi anche di altri utenti
         u # fornisce nome dell'utente che lo ha lanciato + ora di esecuzione
         x # mostra anche i processi "demoni" (senza terminale di controllo)
-        aux # mostra tutti i procesi + chi li ha aperti
+        aux # mostra tutti i processi + chi li ha eseguiti
